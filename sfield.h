e83727c84db97c0a8256e124ed888af50f23b455
LordYellow <raphaelstraub@live.de>
2018-03-15 18:02:24 +0100

    Will be able to solve soon!

--- sfield.h
+++ sfield.h
@@ -2,8 +2,48 @@
 #define SFIELD_H
 
 #include <vector>
+#include <iostream>
 
 using namespace std;
+
+/**
+  @brief removes the impossible numbers from pnumsoffield
+
+  @param pnumsoffield the possible numbers of the field
+  @param field is the sudoku field
+  @param a is the first coordinate from the field you wanna clear;
+  @param b is the seccond coordinate from the field you wanna clear;
+
+  @returns pnumsoffield
+*/
+vector<vector<vector<int>>> cleardoubles(vector<vector<vector<int>>> pnumsoffield,vector<vector<int>> field, int a, int b){
+  if(pnumsoffield[a][b].size() > 1){
+  vector<int> h; vector<int> v; vector<int> s;
+  for(int i = 0; i < 9; i++){
+    h.push_back(field[a][i]);
+    v.push_back(field[i][b]);
+  }
+  int oa = a, ob = b;
+  if(a < 3){a = 0;}else if(a < 6){a = 3;}else{a = 6;}
+  if(b < 3){b = 0;}else if(b < 6){b = 3;}else{b = 6;}
+  for(int d = 0; d < 3; d++){
+    for(int c = 0; c < 3; c++){
+      s.push_back(field[a+c][b+d]);
+    }
+  }
+  vector<vector<int>> hvs = {h,v,s};
+  for(y:hvs){
+    for(x:y){
+      for(int i = 0; i < pnumsoffield[oa][ob].size(); i++){
+        if(pnumsoffield[oa][ob][i] == x){
+          pnumsoffield[oa][ob].erase(pnumsoffield[oa][ob].begin()+i);
+        }
+      }
+    }
+  }
+}
+  return pnumsoffield;
+}
 
 /**
   @brief fills the pnumsoffield with all numbers if field in sudoku is empty
@@ -36,7 +76,12 @@ vector<vector<vector<int>>> getpnumsoffield(vector<vector<int>> field){
 vector<vector<int>> solvefield(vector<vector<int>> field){
   vector<vector<vector<int>>> pnumsoffield;
   pnumsoffield = getpnumsoffield(field);
-
+  for(int a = 0; a < 9; a++){
+    for(int b = 0; b < 9; b++){
+      cout << a << " " << b << endl;
+      pnumsoffield = cleardoubles(pnumsoffield, field,a,b);
+    }
+  }
 
   return field;
 }
